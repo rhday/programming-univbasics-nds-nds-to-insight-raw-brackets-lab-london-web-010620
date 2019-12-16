c550266cc2a8a6c006821576f10251548126d734
Ruari Day <ruariday@gmail.com>
2019-12-16 13:59:22 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -14,7 +14,7 @@ def directors_totals(nds)
       total_grossed += films[film_index][:worldwide_gross]
       film_index += 1
     end
-    result[name]
+    result[name] = total_grossed
   end
 
   nil
